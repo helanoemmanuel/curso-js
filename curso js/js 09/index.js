b627c9eaf62618ff3126a665c31b994f53076def
Helano Emmanuel <134344128+helanoemmanuel@users.noreply.github.com>
2023-06-21 12:01:38 -0300

    Add files via upload

---
+++ curso js/js 09/index.js
@@ -0,0 +1,10 @@
+ Node.js
+var vel = "60.5"
+console.log (`A velocidade do seu carro é ${vel}`)
+      
+ if (vel>60) {
+    Console.log ('Voce ultrapassou a velocidade permitida. MULTADO!')
+ }
+console.log ('Dirija sempre usando cinto de segurança')
+ 
+
